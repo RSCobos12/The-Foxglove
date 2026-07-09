26d30c8719c21756237f5672a58fd73284c94bf9
Scott Cobos <cobosfamily@Mac.lan>
2026-07-08 21:35:04 -0700

    Complete Courses page v1.0

--- js/script.js
+++ js/script.js
@@ -54,3 +54,26 @@ winnerCards.forEach((card) => {
     card.classList.add("active");
   });
 });
+
+const courseCards = document.querySelectorAll(".course-card");
+
+const courseCardObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry, index) => {
+      if (entry.isIntersecting) {
+        setTimeout(() => {
+          entry.target.classList.add("course-card-visible");
+        }, index * 100);
+
+        courseCardObserver.unobserve(entry.target);
+      }
+    });
+  },
+  {
+    threshold: 0.18,
+  }
+);
+
+courseCards.forEach((card) => {
+  courseCardObserver.observe(card);
+});
